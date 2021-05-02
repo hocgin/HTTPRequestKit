a18b6818690cfa8f6f3e94dcb9d8c13b45167462
saroar <saroar9@gmail.com>
2021-05-02 21:16:31 +0300

    Here we go 🚀

--- Package.swift
+++ Package.swift
@@ -4,25 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "HttpRequest",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "HttpRequest",
-            targets: ["HttpRequest"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "HttpRequest",
-            dependencies: []),
-        .testTarget(
-            name: "HttpRequestTests",
-            dependencies: ["HttpRequest"]),
-    ]
+  name: "HttpRequest",
+  platforms: [
+    .iOS(.v14),
+    .macOS(.v10_15),
+    .tvOS(.v10),
+    .watchOS(.v3)
+  ],
+  products: [
+    .library(name: "HttpRequest", targets: ["HttpRequest"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "HttpRequest",
+      dependencies: []),
+    .testTarget(
+      name: "HttpRequestTests",
+      dependencies: ["HttpRequest"]),
+  ]
 )
