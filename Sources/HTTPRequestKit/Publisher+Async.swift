52f61a2f6736fa2223a1f56d44e2abc2b6d2192c
hocgin <hocgin@gmail.com>
2025-06-17 15:39:11 +0800

    dd

--- Sources/HTTPRequestKit/Publisher+Async.swift
+++ Sources/HTTPRequestKit/Publisher+Async.swift
@@ -10,20 +10,44 @@ import Foundation
 
 public extension Publisher where Output: Sendable, Failure == Error {
     func async() async throws -> Output {
-        var cancellable: AnyCancellable?
-        defer { cancellable?.cancel() }
-
-        return try await withCheckedThrowingContinuation { continuation in
-            cancellable = self.first().sink(
-                receiveCompletion: { completion in
-                    if case let .failure(error) = completion {
+        try await withCheckedThrowingContinuation { continuation in
+            var cancellable: AnyCancellable?
+            var finishedWithoutValue = true
+            cancellable = first()
+                .sink { result in
+                    switch result {
+                    case .finished:
+                        if finishedWithoutValue {
+                            continuation.resume(throwing: HTTPRequest.HRError.nonHTTPResponse)
+                        }
+                    case let .failure(error):
                         continuation.resume(throwing: error)
                     }
-                },
-                receiveValue: { value in
-                    continuation.resume(returning: value)
+                    cancellable?.cancel()
+                } receiveValue: { value in
+                    finishedWithoutValue = false
+                    continuation.resume(with: .success(value))
                 }
-            )
         }
     }
+
+//    func async() async throws -> Output {
+//        var cancellable: AnyCancellable?
+//        defer { cancellable?.cancel() }
+//
+//
+//
+//        return try await withCheckedThrowingContinuation { continuation in
+//            cancellable = self.first().sink(
+//                receiveCompletion: { completion in
+//                    if case let .failure(error) = completion {
+//                        continuation.resume(throwing: error)
+//                    }
+//                },
+//                receiveValue: { value in
+//                    continuation.resume(returning: value)
+//                }
+//            )
+//        }
+//    }
 }
