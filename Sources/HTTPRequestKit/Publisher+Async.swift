6405e8193b6279dfa372751f34f8201526ace09a
hocgin <hocgin@gmail.com>
2025-07-24 15:13:24 +0800

    dev

--- Sources/HTTPRequestKit/Publisher+Async.swift
+++ Sources/HTTPRequestKit/Publisher+Async.swift
@@ -10,44 +10,25 @@ import Foundation
 
 public extension Publisher where Output: Sendable, Failure == Error {
     func async() async throws -> Output {
-        try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            var finishedWithoutValue = true
-            cancellable = first()
-                .sink { result in
-                    switch result {
-                    case .finished:
-                        if finishedWithoutValue {
-                            continuation.resume(throwing: HTTPRequest.HRError.nonHTTPResponse)
-                        }
-                    case let .failure(error):
+        var cancellable: AnyCancellable?
+        defer { cancellable?.cancel() }
+
+        return try await withCheckedThrowingContinuation { continuation in
+            var isResumed = false
+            cancellable = self.first().sink(
+                receiveCompletion: { completion in
+                    guard !isResumed else { return }
+                    if case let .failure(error) = completion {
+                        isResumed = true
                         continuation.resume(throwing: error)
                     }
-                    cancellable?.cancel()
-                } receiveValue: { value in
-                    finishedWithoutValue = false
-                    continuation.resume(with: .success(value))
+                },
+                receiveValue: { value in
+                    guard !isResumed else { return }
+                    isResumed = true
+                    continuation.resume(returning: value)
                 }
+            )
         }
     }
-
-//    func async() async throws -> Output {
-//        var cancellable: AnyCancellable?
-//        defer { cancellable?.cancel() }
-//
-//
-//
-//        return try await withCheckedThrowingContinuation { continuation in
-//            cancellable = self.first().sink(
-//                receiveCompletion: { completion in
-//                    if case let .failure(error) = completion {
-//                        continuation.resume(throwing: error)
-//                    }
-//                },
-//                receiveValue: { value in
-//                    continuation.resume(returning: value)
-//                }
-//            )
-//        }
-//    }
 }
