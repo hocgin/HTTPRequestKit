7b7181ee2273760753281b19403b00c81a2f1d03
hocgin <hocgin@gmail.com>
2025-07-24 15:23:31 +0800

    dev

--- Sources/HTTPRequestKit/Publisher+Async.swift
+++ Sources/HTTPRequestKit/Publisher+Async.swift
@@ -10,22 +10,25 @@ import Foundation
 
 public extension Publisher where Output: Sendable, Failure == Error {
     func async() async throws -> Output {
-        var cancellable: AnyCancellable?
-        defer { cancellable?.cancel() }
-
         return try await withCheckedThrowingContinuation { continuation in
+            var cancellable: AnyCancellable?
             var isResumed = false
+
             cancellable = self.first().sink(
                 receiveCompletion: { completion in
                     guard !isResumed else { return }
+                    isResumed = true
+
+                    cancellable?.cancel() // ✅ 放到 resume 后面
                     if case let .failure(error) = completion {
-                        isResumed = true
                         continuation.resume(throwing: error)
                     }
                 },
                 receiveValue: { value in
                     guard !isResumed else { return }
                     isResumed = true
+
+                    cancellable?.cancel() // ✅ 放到 resume 后面
                     continuation.resume(returning: value)
                 }
             )
